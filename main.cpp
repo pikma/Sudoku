a627586bb77ca4d74ab43494bdaf2af64ef65bd1
Pierre Kreitmann <pierre.kreitmann@gmail.com>
2011-10-13 19:41:58 -0700

    Better usage explanations

--- main.cpp
+++ main.cpp
@@ -6,13 +6,21 @@ int main(int argc, char ** argv)
 {
     const char * filename = NULL;
     if (argc < 2) {
+        std::cout << "Usage: " << argv[0] << " fileName" << std::endl << std::endl ;
+        std::cout << "For examples or sudoku files, look at:" << std::endl;
+        std::cout << "easySudoku.txt - a very easy Sudoku" << std::endl;
+        std::cout << "leMondeSudoku.txt - a sudoku from the daily newspaper Le Monde, supposedly hard" << std::endl;
+        std::cout << "sudokuHardest.txt - the hardest sudoku known to have a unique solution (try it ;) )" << std::endl;
+
         filename = "easySudoku.txt";
+        std::cout << std::endl;
+        std::cout << "By default, I'll use easySudoku.txt" << std::endl << std::endl;
     }
     else {
         filename = argv[1];
     }
     Board b(filename);
-    std::cout << "Initial configuration:\n" << b.toString() << std::endl;
+    std::cout << "Initial board:\n" << b.toString() << std::endl;
     Backtrack backtrack;
     backtrack.explore(b);
 
